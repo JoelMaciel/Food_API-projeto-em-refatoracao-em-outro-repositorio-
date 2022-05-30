5822d3c8592dabdd97431b33f88c414f1563620d
Joel Maciel <soldado25461@gmail.com>
2022-05-30 17:49:17 -0300

    exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/joel/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/joel/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -127,8 +127,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String path = joinPath(ex.getPath());
 		
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
-		String detail = String.format("A propriedade '%s' não existe" 
-				+ "Corrija ou remova essa propriedade e tente novamente", path);
+		String detail = String.format("A propriedade '%s' não existe." 
+				+  " Corrija ou remova essa propriedade e tente novamente", path);
 		
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
 		
