82a35e414475c1d68ad91df8dfcd28afdb0cf70b
Joel Maciel <soldado25461@gmail.com>
2022-06-08 18:19:36 -0300

    Configurando Maven Failsafe Plugin no projeto bug  do javadoc corrigido

--- src/test/java/com/joel/food/CadastroCozinhaIntegrationT.java
+++ src/test/java/com/joel/food/CadastroCozinhaIntegrationT.java
@@ -1,54 +1,66 @@
 package com.joel.food;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import javax.validation.ConstraintViolationException;
-
-import org.junit.jupiter.api.Assertions;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.joel.food.domain.exception.CozinhaNaoEncontradaException;
 import com.joel.food.domain.exception.EntidadeEmUsoException;
-import com.joel.food.domain.model.Cozinha;
 import com.joel.food.domain.service.CadastroCozinhaService;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-class CadastroCozinhaIntegrationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CadastroCozinhaIntegrationT {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.given()
+		.basePath("/cozinhas")
+		.port(port)
+		.accept(ContentType.JSON)
+		.when().get().then().statusCode(HttpStatus.SC_OK);
+	}
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
-
-	@Test
-	public void testarCadastroCozinhaComSucesso() {
-
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome("Chinesa");
-
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-
-		assertThat(novaCozinha).isNotNull();
-		assertThat(novaCozinha.getId()).isNotNull();
-	}
-
-	@Test
-	public void testarCadastroCozinhaSemNome() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome(null);
-
-		ConstraintViolationException erroEsperado = Assertions.assertThrows(ConstraintViolationException.class, () -> {
-			cadastroCozinha.salvar(novaCozinha);
-
-		});
-		assertThat(erroEsperado).isNotNull();
-
-	}
+//
+//	@Test
+//	public void testarCadastroCozinhaComSucesso() {
+//
+//		Cozinha novaCozinha = new Cozinha();
+//		novaCozinha.setNome("Chinesa");
+//
+//		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+//
+//		assertThat(novaCozinha).isNotNull();
+//		assertThat(novaCozinha.getId()).isNotNull();
+//	}
+//
+//	@Test
+//	public void testarCadastroCozinhaSemNome() {
+//		Cozinha novaCozinha = new Cozinha();
+//		novaCozinha.setNome(null);
+//
+//		ConstraintViolationException erroEsperado = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+//			cadastroCozinha.salvar(novaCozinha);
+//
+//		});
+//		assertThat(erroEsperado).isNotNull();
+//
+//	}
 	@Test
 	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
 		EntidadeEmUsoException exception = assertThrows(EntidadeEmUsoException.class, () -> {
