9d2b5c22e5ce00de8eda458f28125537a6d54ae3
Joel Maciel <soldado25461@gmail.com>
2022-07-06 21:56:22 -0300

    implementando o endpoint de emissao de pedidos

--- src/main/java/com/joel/food/api/controller/PedidoController.java
+++ src/main/java/com/joel/food/api/controller/PedidoController.java
@@ -2,17 +2,28 @@ package com.joel.food.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.joel.food.api.assembler.PedidoInputDisassembler;
 import com.joel.food.api.assembler.PedidoModelAssembler;
 import com.joel.food.api.assembler.PedidoResumoModelAssembler;
 import com.joel.food.api.model.PedidoModel;
 import com.joel.food.api.model.PedidoResumoModel;
+import com.joel.food.api.model.input.PedidoInput;
+import com.joel.food.domain.exception.EntidadeNaoEncontradaException;
+import com.joel.food.domain.exception.NegocioException;
 import com.joel.food.domain.model.Pedido;
+import com.joel.food.domain.model.Usuario;
 import com.joel.food.domain.repository.PedidoRepository;
 import com.joel.food.domain.service.EmissaoPedidoService;
 
@@ -32,6 +43,9 @@ public class PedidoController {
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
 	
+	@Autowired
+	private PedidoInputDisassembler pedidoInputDisassembler;
+	
 	@GetMapping
 	public List<PedidoResumoModel> listar() {
 		List<Pedido> todosPedidos = pedidoRepository.findAll();
@@ -43,6 +57,23 @@ public class PedidoController {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
 		
 		return pedidoModelAssembler.toModel(pedido);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PedidoModel adicionar(@Valid @RequestBody PedidoInput pedidoInput) {
+		try {
+			Pedido novoPedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
+			
+			novoPedido.setCliente(new Usuario());
+			novoPedido.getCliente().setId(1L);
+			
+			novoPedido = emissaoPedido.emitir(novoPedido);
+			
+			return pedidoModelAssembler.toModel(novoPedido);
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
 	}
 }
 
--- src/main/java/com/joel/food/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/joel/food/core/modelmapper/ModelMapperConfig.java
@@ -5,25 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.joel.food.api.model.EnderecoModel;
+import com.joel.food.api.model.input.ItemPedidoInput;
 import com.joel.food.domain.model.Endereco;
+import com.joel.food.domain.model.ItemPedido;
 
 @Configuration
 public class ModelMapperConfig {
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
-		
+
 		var modelMapper = new ModelMapper();
 		
-//		modelMapper.createTypeMap(Restaurante.class	, RestauranteModel.class)
-//		.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setTaxaFrete);
-		
+		//quando for fazer a conversao de ItemPedidoInput para ItemPedido o atributo Id será ignorado.
+		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+		.addMappings(mapper -> mapper.skip(ItemPedido::setId));
+
 		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
-		
-		enderecoToEnderecoModelTypeMap.<String>addMapping(
-				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(), 
+
+		enderecoToEnderecoModelTypeMap.<String>addMapping(enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
 				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
-		
+
 		return modelMapper;
 	}
 
