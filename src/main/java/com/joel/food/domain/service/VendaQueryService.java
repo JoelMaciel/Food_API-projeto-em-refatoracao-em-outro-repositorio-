5898d8173f7fa7f6b08529a690eb8adffe082d45
Joel Maciel <soldado25461@gmail.com>
2022-08-07 11:58:27 -0300

    Tratando time offset na agregação de vendas diárias por data

--- src/main/java/com/joel/food/domain/service/VendaQueryService.java
+++ src/main/java/com/joel/food/domain/service/VendaQueryService.java
@@ -7,7 +7,7 @@ import com.joel.food.domain.model.dto.VendaDiaria;
 
 public interface VendaQueryService {
 	
-	List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro);
+	List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
 
 
 }
