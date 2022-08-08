3dd33ea3cd6aa8db4c927f22919d692495b8beea
Joel Maciel <soldado25461@gmail.com>
2022-08-08 17:32:28 -0300

    Validando o content type do arquivo

--- src/main/java/com/joel/food/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/joel/food/api/model/input/FotoProdutoInput.java
@@ -1,9 +1,12 @@
 package com.joel.food.api.model.input;
 
+
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.joel.food.core.validation.FileContentType;
 import com.joel.food.core.validation.FileSize;
 
 import lombok.Getter;
@@ -15,8 +18,15 @@ public class FotoProdutoInput {
 
 	@NotNull
 	@FileSize(max = "500KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 	
 	@NotNull
 	private String descricao;
 }
+
+
+
+
+
+
