3a1067c466fc04a0b0e66e3c7e481932bbcf9667
Joel Maciel <soldado25461@gmail.com>
2022-08-12 07:57:32 -0300

    Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/joel/food/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/joel/food/infrastructure/service/storage/S3FotoStorageService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.joel.food.core.storage.StorageProperties;
@@ -47,14 +48,24 @@ public class S3FotoStorageService implements FotoStorageService {
 			throw new StorageException("Não foi possível enviar arquivo para Amazon S3" , e);
 		}
 	}
+	
+	@Override
+	public void remover(String nomeArquivo) {
+		try {
+			var caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+			
+			var deleteObjectRequest = new DeleteObjectRequest(
+					storageProperties.getS3().getBucket(), caminhoArquivo);
+			amazonS3.deleteObject(deleteObjectRequest);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir arquivo na Amazon S3", e);
+		}
+	}
 
 	private String getCaminhoArquivo(String nomeArquivo) {
 		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
 	}
 
-	@Override
-	public void remover(String nomeArquivo) {
-	}
 
 }
 
