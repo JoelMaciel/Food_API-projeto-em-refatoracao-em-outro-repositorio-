3ccf963a50ddd8be521ed2e2af54fcd9e76471c7
Joel Maciel <soldado25461@gmail.com>
2022-08-20 16:51:11 -0300

    Descrevendo o modelo de representação de problema

--- src/main/java/com/joel/food/api/exceptionhandler/Problem.java
+++ src/main/java/com/joel/food/api/exceptionhandler/Problem.java
@@ -6,26 +6,46 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+@ApiModel("Problema")
 @JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
 	
+	@ApiModelProperty(example =  "400")
 	private Integer status;
-	private String type;
-	private String title;
-	private String detail;
-	private String userMessage;
+	
+	@ApiModelProperty(example =  "2022-08-01T18:09:02.70844Z")
 	private OffsetDateTime timestamp;
+	
+	@ApiModelProperty(example =  "http://food.api.com.br/dados-invalidos")
+	private String type;
+	
+	@ApiModelProperty(example =  "Dados invalidos")
+	private String title;
+	
+	@ApiModelProperty(example =  "Um ou mais campos estão inválidos. Faça o preenchimento correto.")
+	private String detail;
+	
+	@ApiModelProperty(example =  "Um ou mais campos estão inválidos. Faça o preenchimento correto.")
+	private String userMessage;
+	
+	@ApiModelProperty(example = "Objetos os campos que geraram o erro")
 	private List<Object> objects;
 	
+	@ApiModel("Objeto problema")
 	@Getter
 	@Builder
 	public static class Object {
+		
+		@ApiModelProperty(example = "preco")
 		private String name;
+		@ApiModelProperty(example = "O preço é obrigatório.")
 		private String userMessage;
 	}
 	
