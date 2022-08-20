84b6d37bcf0b4499ae5814938c195c0c33686c08
Joel Maciel <soldado25461@gmail.com>
2022-08-20 12:39:46 -0300

    Descrevendo restrições de validação de propriedades do modelo

--- src/main/java/com/joel/food/api/model/input/CidadeInput.java
+++ src/main/java/com/joel/food/api/model/input/CidadeInput.java
@@ -1,19 +1,19 @@
 package com.joel.food.api.model.input;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class CidadeInput {
 
-	@ApiModelProperty(example = "Uberlandia")
-	@NonNull
+	@ApiModelProperty(example = "Uberlandia", required = true)
+	@NotBlank
 	private String nome;
 	
 	@NotNull
