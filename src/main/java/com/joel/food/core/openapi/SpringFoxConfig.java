ad228b1845202c1c4c53d7a0daacf8038eaf6fba
Joel Maciel <soldado25461@gmail.com>
2022-08-20 15:45:31 -0300

    Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/joel/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/joel/food/core/openapi/SpringFoxConfig.java
@@ -3,16 +3,22 @@ package com.joel.food.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -20,25 +26,41 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.OAS_30).select()
-				.apis(RequestHandlerSelectors.basePackage("com.joel.food.api"))
-				.paths(PathSelectors.any())
-				.build()
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+					.apis(RequestHandlerSelectors.basePackage("com.joel.food.api"))
+					.paths(PathSelectors.any())
+					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 				.apiInfo(apiInfo())
-				.tags(new Tag("Cidades", "Gerencia as cidades") );
-	}
-	
-	public ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Food API")
-				.description("API aberta para clientes e restaurantes")
-				.version("1")
-				.contact(new Contact("Joel Maciel", 
-						"https://www.linkedin.com/in/joel-maciel-dev-java-back-end-spring-framework/",
-						"jmviana37@gmail.com"))
-				.build();
+				.tags(new Tag("Cidades", "Gerencia as cidades"));
 	}
 
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Erro interno do Servidor")
+						.build(),
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+						.description("Recurso não possui representação que pode ser aceita pelo consumidor")
+						.build()
+		);
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("AlgaFood API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("AlgaWorks",
+						"https://www.linkedin.com/in/joel-maciel-dev-java-back-end-spring-framework",
+						 "jmviana37@gmail.com"))
+				.build();
+	}
+	
 }
 
 
