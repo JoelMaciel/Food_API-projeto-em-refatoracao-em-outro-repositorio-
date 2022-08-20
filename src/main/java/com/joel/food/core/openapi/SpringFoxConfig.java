d460a597b0bb185b6379d6c51f0a7eac44833d1d
Joel Maciel <soldado25461@gmail.com>
2022-08-20 09:33:50 -0300

    Iniciando Swagger - Descrevendo informações da API na documentação

--- src/main/java/com/joel/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/joel/food/core/openapi/SpringFoxConfig.java
@@ -2,7 +2,12 @@ package com.joel.food.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -11,10 +16,30 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.OAS_30)
-				.select()
-					.apis(RequestHandlerSelectors.any())
-					.build();
+		return new Docket(DocumentationType.OAS_30).select()
+				.apis(RequestHandlerSelectors.basePackage("com.joel.food.api"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
 	}
 	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Food API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("Joel Maciel", 
+						"https://www.linkedin.com/in/joel-maciel-dev-java-back-end-spring-framework/",
+						"jmviana37@gmail.com"))
+				.build();
+	}
+
 }
+
+
+
+
+
+
+
+
