0cb35c1eccf6e8549d56ddb2b220dcf3e350ecdf
Joel Maciel <soldado25461@gmail.com>
2022-08-21 16:33:47 -0300

    Ignorando tipos de parâmetros de operações na documentação

--- src/main/java/com/joel/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/joel/food/core/openapi/SpringFoxConfig.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import com.fasterxml.classmate.TypeResolver;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -58,6 +59,7 @@ public class SpringFoxConfig {
 	        .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
 	        .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 	        .additionalModels(typeResolver.resolve(Problem.class))
+	        .ignoredParameterTypes(ServletWebRequest.class)
 	        .alternateTypeRules(AlternateTypeRules.newRule(
 	        		typeResolver.resolve(Page.class,CozinhaModel.class), CozinhasModelOpenApi.class))
 	        .apiInfo(apiInfo())
