9c37f79a3718eeea89bd830b3e4cf0dceb6ef723
Joel Maciel <soldado25461@gmail.com>
2022-08-23 17:55:41 -0300

    descrevendo documentação de endpoints de produtos

--- src/main/java/com/joel/food/api/controller/RestauranteProdutoController.java
+++ src/main/java/com/joel/food/api/controller/RestauranteProdutoController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.joel.food.api.assembler.ProdutoInputDisassembler;
 import com.joel.food.api.assembler.ProdutoModelAssembler;
 import com.joel.food.api.model.ProdutoModel;
 import com.joel.food.api.model.input.ProdutoInput;
+import com.joel.food.api.openapi.controller.RestauranteProdutoControllerOpenApi;
 import com.joel.food.domain.model.Produto;
 import com.joel.food.domain.model.Restaurante;
 import com.joel.food.domain.repository.ProdutoRepository;
@@ -27,8 +29,8 @@ import com.joel.food.domain.service.CadastroProdutoService;
 import com.joel.food.domain.service.CadastroRestauranteService;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteId}/produtos")
-public class RestauranteProdutoController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/produtos" , produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteProdutoController  implements RestauranteProdutoControllerOpenApi{
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
