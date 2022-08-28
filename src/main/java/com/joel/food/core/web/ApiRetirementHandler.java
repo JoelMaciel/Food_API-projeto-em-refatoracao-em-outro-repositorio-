b8b520c95fd30a794c3bb852bb0d160ce27c9921
Joel Maciel <soldado25461@gmail.com>
2022-08-28 18:33:12 -0300

    Desligando uma versão da API

--- src/main/java/com/joel/food/core/web/ApiRetirementHandler.java
+++ src/main/java/com/joel/food/core/web/ApiRetirementHandler.java
@@ -3,23 +3,23 @@ package com.joel.food.core.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
-public class ApiDeprecationHandler implements HandlerInterceptor {
+public class ApiRetirementHandler implements HandlerInterceptor {
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+	public boolean preHandle(HttpServletRequest request, 
+	                         HttpServletResponse response, Object handler)
 			throws Exception {
 		if (request.getRequestURI().startsWith("/v1/")) {
-			response.addHeader("X-Food_API-Deprecated",
-					"Essa versão da API está depreciada e deixará de existir a partir de 30/12/2022."
-							+ "Use a versão mais atual da API.");
+			response.setStatus(HttpStatus.GONE.value());
+			return false;
 		}
 
 		return true;
 	}
-
 }
 
