083698ef315c8fda71f8818fa84b6630e8b1eee7
Joel Maciel <soldado25461@gmail.com>
2022-08-29 11:05:10 -0300

    Configurando autenticação de usuários em memória

--- src/main/java/com/joel/food/core/security/WebSecurityConfig.java
+++ src/main/java/com/joel/food/core/security/WebSecurityConfig.java
@@ -1,14 +1,30 @@
 package com.joel.food.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("Joel")
+			.password(passwordEncoder().encode("123456"))
+			.roles("ADMIN")
+		.and()
+			.withUser("Maciel")
+			.password(passwordEncoder().encode("456789"))
+			.roles("ADMIN");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -26,7 +42,20 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 		.csrf().disable();
 		
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 }
+
+
+
+
+
+
+
 
 
 
