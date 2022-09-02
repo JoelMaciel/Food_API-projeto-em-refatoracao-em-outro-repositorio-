961bd25b43a78a6061139191806d65e7e19abb74
Joel Maciel <soldado25461@gmail.com>
2022-09-02 17:47:08 -0300

    Implementando o endpoint do JSON Web Key Set (JWKS)

---
+++ src/main/java/com/joel/food/core/security/authorizationserver/JwtSetController.java
@@ -0,0 +1,23 @@
+package com.joel.food.core.security.authorizationserver;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nimbusds.jose.jwk.JWKSet;
+
+@RestController
+public class JwtSetController {
+	
+	@Autowired
+	private JWKSet jwkSet;
+	
+	@GetMapping("/.well-know/jwks.json")
+	public Map<String, Object> keys(){
+		return this.jwkSet.toJSONObject();
+		
+	}
+
+}
