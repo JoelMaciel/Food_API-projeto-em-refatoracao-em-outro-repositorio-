50cfa8f198e5d8b0b3774d0c2a65bcb9a3830dab
Joel Maciel <soldado25461@gmail.com>
2022-09-02 16:54:16 -0300

    Ajustando a documentação da API para suporte a OAuth2

--- src/main/java/com/joel/food/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/joel/food/core/springfox/SpringFoxConfig.java
@@ -55,16 +55,24 @@ import com.joel.food.api.v2.openapi.model.CozinhasModelV2OpenApi;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
 import springfox.documentation.service.Response;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -124,6 +132,10 @@ public class SpringFoxConfig {
 					.alternateTypeRules(AlternateTypeRules.newRule(
 					        typeResolver.resolve(CollectionModel.class, PermissaoModel.class),
 					        PermissoesModelOpenApi.class))
+					  .securityContexts(Arrays.asList(securityContext()))
+			           .securitySchemes(List.of(authenticationScheme()))
+			           .securityContexts(List.of(securityContext()))
+					
 				.apiInfo(apiInfoV1())
 				.tags(new Tag("Cidades", "Gerencia as cidades"),
 						new Tag("Grupos", "Gerencia os grupos de usuários"),
@@ -136,10 +148,41 @@ public class SpringFoxConfig {
 						new Tag("Usuários", "Gerencia os usuários"),
 						 new Tag("Estatísticas", "Estatísticas da AlgaFood"),
 						 new Tag("Permissões", "Gerencia as permissões"));
-
-
 	        
 	  }
+	 
+	 private SecurityScheme securityScheme() {
+		 return new OAuthBuilder()
+				 .name("Food")
+				 .grantTypes(grantTypes())
+				 .scopes(scopes())
+				 .build();
+	 }
+	 
+	 private SecurityContext securityContext() {
+		  return SecurityContext.builder()
+		        .securityReferences(securityReference()).build();
+		}
+
+		private List<SecurityReference> securityReference() {
+		  AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		  AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		  authorizationScopes[0] = authorizationScope;
+		  return List.of(new SecurityReference("Authorization", authorizationScopes));
+		}
+
+		private HttpAuthenticationScheme authenticationScheme() {
+		  return HttpAuthenticationScheme.JWT_BEARER_BUILDER.name("Authorization").build();
+		}
+	 
+	 private List<GrantType> grantTypes() {
+		 return Arrays.asList(new ResourceOwnerPasswordCredentialsGrant("/oauth/token"));
+	 }
+	 
+	 private List<AuthorizationScope> scopes(){
+		 return Arrays.asList(new AuthorizationScope("READ", "Acesso de leitura"),
+				 new AuthorizationScope("WRITE","Acesso de leitura"));
+	 }
 	 
 	    @Bean
 		public Docket apiDocketV2() {
@@ -169,6 +212,9 @@ public class SpringFoxConfig {
 						        typeResolver.resolve(CollectionModel.class, CidadeModelV2.class),
 						        CidadesModelV2OpenApi.class))
 						.apiInfo(apiInfoV2())
+						  .securityContexts(Arrays.asList(securityContext()))
+				           .securitySchemes(List.of(authenticationScheme()))
+				           .securityContexts(List.of(securityContext()))
 						        
 						.tags(new Tag("Cidades", "Gerencia as cidades"),
 						        new Tag("Cozinhas", "Gerencia as cozinhas"));
